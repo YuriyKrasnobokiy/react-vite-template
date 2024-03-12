775a77016fb996abca0a02e4c4418c7aba560570
YuriyKrasnobokiy <91632730+YuriyKrasnobokiy@users.noreply.github.com>
2024-03-12 21:19:49 +0000

    Deploying to gh-pages from @ YuriyKrasnobokiy/react-vite-template@15b0c658096c3b73ebf4414fd49409e2e27cf962 🚀

---
+++ assets/Catalog-CegwcZjB.js
@@ -0,0 +1,1 @@
+import{j as a}from"./index-Ctr9Vn7h.js";const s=()=>a.jsx("div",{children:"This is a catalog page"});export{s as default};
---
+++ assets/Home-AKTzc7lk.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-Ctr9Vn7h.js";const o=()=>e.jsx("div",{children:"It`s Home Page"});export{o as default};
