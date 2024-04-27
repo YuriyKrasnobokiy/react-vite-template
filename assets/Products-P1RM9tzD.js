8e2af3885866be691d389c7731cefc8d7a07772d
YuriyKrasnobokiy <91632730+YuriyKrasnobokiy@users.noreply.github.com>
2024-04-27 08:53:49 +0000

    Deploying to gh-pages from @ YuriyKrasnobokiy/react-vite-template@9490312a5124ab9a8c146e45136cc3c1bacc4db5 🚀

--- assets/Products-P1RM9tzD.js
+++ assets/Products-P1RM9tzD.js
@@ -1,3 +1,3 @@
-import{u as o,j as s}from"./index-DwVGdulQ.js";const r=o.p`
+import{u as o,j as s}from"./index-B16tUohu.js";const r=o.p`
   color: ${t=>t.theme.colors.textColor};
 `,c=()=>s.jsx("div",{className:"container",children:s.jsx(r,{children:"This is a Products page"})});export{c as default};
