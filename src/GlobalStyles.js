d14c73b8257cdbd5d72e44d5b8b36aec2de76619
Yuriy Krasnobokiy <91632730+YuriyKrasnobokiy@users.noreply.github.com>
2025-07-28 10:21:56 +0300

    Update GlobalStyles.js

--- src/GlobalStyles.js
+++ src/GlobalStyles.js
@@ -2,6 +2,10 @@ import { createGlobalStyle } from "styled-components";
 
 const GlobalStyles = createGlobalStyle`
 
+ *, *::before, *::after {
+    box-sizing: border-box;
+  }
+
   body {
     margin: 0;
     font-family: "Manrope", sans-serif;
